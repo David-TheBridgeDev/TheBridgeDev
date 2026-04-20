0dde926d68c849237638de3f6282d44c620afa32
david.ramos <david.ramos.perdomo@seidor.com>
2026-04-20 12:16:17 +0200

    refactor

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -12,27 +12,10 @@ import { ContentContainerComponent } from '../content/content-container.componen
   imports: [HeaderComponent, ContentContainerComponent, FooterComponent],
 })
 export class HomeComponent implements OnInit {
-  private selectedPlan: 'standard' | 'premium' | null = null;
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
   ) {}
 
-  ngOnInit(): void {
-    this.selectedPlan = 'standard';
-  }
-
-  scrollToElement(id: string): void {
-    const element = document.getElementById(id);
-    if (element) {
-      const yOffset = 0; // desplazamiento negativo
-      const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
-
-      window.scrollTo({
-        top: y,
-        behavior: 'smooth',
-      });
-    }
-  }
+  ngOnInit(): void {}
 }
