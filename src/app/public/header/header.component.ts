0921ce017a222270333e22e25e5c10d00972871c
david.ramos <david.ramos.perdomo@seidor.com>
2026-04-20 13:54:42 +0200

    feat: change language btns

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private translate: TranslateService) {}
+
+  get currentLang(): string {
+    return this.translate.currentLang || 'en';
+  }
+
+  changeLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
+}
