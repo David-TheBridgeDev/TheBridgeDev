02d089ada7e1393ba11c0c36a4755761d434cceb
david.ramos <david.ramos.perdomo@seidor.com>
2026-04-20 11:15:10 +0200

    refactor: decompose content container and enhance 404 page aesthetic

--- src/app/@shared/components/page-not-found/page-not-found.component.ts
+++ src/app/@shared/components/page-not-found/page-not-found.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NGXLogger } from 'ngx-logger';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { AppRoutes } from '../../models/appRoutes';
 
 @Component({
     selector: 'app-page-not-found',
     templateUrl: './page-not-found.component.html',
     styleUrls: ['./page-not-found.component.scss'],
     standalone: true,
+    imports: [RouterLink]
 })
 export class PageNotFoundComponent {
+  protected readonly AppRoutes = AppRoutes;
+
   constructor(
     private log: NGXLogger,
     private route: ActivatedRoute,
@@ -17,7 +21,7 @@ export class PageNotFoundComponent {
 
   navegateLogin() {
     this.router.navigate(['user/unity'], {
-      relativeTo: this.route.parent.parent,
+      relativeTo: this.route.parent?.parent,
       queryParamsHandling: 'preserve',
     });
   }
