7d8a16f53f909c2cc4fde16537b40f2eeb4aad28
david.ramos <david.ramos.perdomo@seidor.com>
2026-04-20 14:34:03 +0200

    feat: restructure footer and migrate featured projects

--- src/app/public/footer/footer.component.ts
+++ src/app/public/footer/footer.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CommonModule } from '@angular/common';
+import { AppRoutes } from '../../@shared/models/appRoutes';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink, MatTooltipModule, CommonModule],
 })
 export class FooterComponent implements OnInit {
+  protected readonly AppRoutes = AppRoutes;
+
   ngOnInit(): void {}
 }
