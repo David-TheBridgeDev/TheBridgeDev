1ad949429185df6f74ac58fa0700372ebd7e33bc
david.ramos <david.ramos.perdomo@seidor.com>
2026-04-20 22:06:44 +0200

    styles

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -10,7 +10,18 @@ import { CommonModule } from '@angular/common';
   imports: [TranslateModule, CommonModule],
 })
 export class HeaderComponent {
-  constructor(private translate: TranslateService) {}
+  osVersion: string;
+  cpuUsage: number;
+  memUsage: number;
+
+  constructor(private translate: TranslateService) {
+    const now = new Date();
+    this.osVersion = `${now.getFullYear()}.${
+      now.getMonth() + 1
+    }.${now.getDate()}`;
+    this.cpuUsage = Math.floor(Math.random() * 8) + 2; // 1-5%
+    this.memUsage = Math.floor(Math.random() * (512 - 64 + 1)) + 64; // 64-512MB
+  }
 
   get currentLang(): string {
     return this.translate.currentLang || 'en';
