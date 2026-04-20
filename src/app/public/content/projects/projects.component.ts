738731fb5735bdbdf5f1d054ac23901ba001ea57
david.ramos <david.ramos.perdomo@seidor.com>
2026-04-20 11:08:32 +0200

    -

--- src/app/public/content/projects/projects.component.ts
+++ src/app/public/content/projects/projects.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { ActivatedRoute, Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { AppRoutes } from '../../../@shared/models/appRoutes';
 
 @Component({
@@ -8,23 +8,8 @@ import { AppRoutes } from '../../../@shared/models/appRoutes';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
   standalone: true,
-  imports: [MatTooltipModule],
+  imports: [MatTooltipModule, RouterLink],
 })
 export class ProjectsComponent {
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-  ) {}
-
-  navegateToDomotic() {
-    this.router.navigate([AppRoutes.HOME_AUTOMATION], {
-      relativeTo: this.route.parent,
-    });
-  }
-
-  navegateToMusic() {
-    this.router.navigate([AppRoutes.MUSIC], {
-      relativeTo: this.route.parent,
-    });
-  }
+  protected readonly AppRoutes = AppRoutes;
 }
