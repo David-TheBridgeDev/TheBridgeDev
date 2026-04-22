59defd126ce374d7d84c2c6f768c6768b528ab93
david.ramos <david.ramos.perdomo@seidor.com>
2026-04-22 13:25:56 +0200

    url maping

--- src/app/projects/music/music-home.component.ts
+++ src/app/projects/music/music-home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CircleOfFifthsComponent } from './circle-of-fifths/circle-of-fifths.component';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
 import { FretboardViewerComponent } from './fretboard/fretboard-viewer/fretboard-viewer.component';
 import { ChordScaleViewerComponent } from './chord-scale-viwer/chord-scale-viewer/chord-scale-viewer.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-music-home',
@@ -17,4 +18,31 @@ import { ChordScaleViewerComponent } from './chord-scale-viwer/chord-scale-viewe
   templateUrl: './music-home.component.html',
   styleUrl: './music-home.component.scss',
 })
-export class MusicHomeComponent {}
+export class MusicHomeComponent implements OnInit {
+  selectedIndex = 0;
+  private tabHashes = ['circle', 'chords', 'scales'];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment) => {
+      const index = this.tabHashes.indexOf(fragment || '');
+      // Ensure we don't update if it's already the selected tab to avoid infinite loops
+      if (index !== -1 && this.selectedIndex !== index) {
+        this.selectedIndex = index;
+      }
+    });
+  }
+
+  onTabChange(index: number) {
+    this.selectedIndex = index;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      fragment: this.tabHashes[index],
+      replaceUrl: true
+    });
+  }
+}
